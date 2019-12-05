50a887c712eebad20884e604b575727d7d0711fa
bioXiaoheng <38220318+bioXiaoheng@users.noreply.github.com>
2019-12-05 15:27:01 -0500

    Updated scripts

--- Empirical_analysis/callAnc_panPan2-hg38.py
+++ Empirical_analysis/callAnc_panPan2-hg38.py
@@ -1,10 +1,10 @@
-#Jan 30, 2019: Adopted to use panPan2 to call substitution to hg18 alignment
-#python script to call ancestral with pantro5 on hg19 coordinates.
+#Jan 30, 2019: Adopted to reformat panPan2 and hg38 alignment for later ancestral calling
+#python script to reformat pantro5 sequence alignment onto hg19 coordinates.
 
 import gzip, os, sys
 from datetime import datetime
 
-ptChrs = ['chr1','chr2a','chr2b'] + ['chr%s'%(i) for i in xrange(3,23)]
+ptChrs = ['chr1','chr2A','chr2B'] + ['chr%s'%(i) for i in xrange(3,23)]
 
 pair = {'A':'T','T':'A','G':'C','C':'G','-':'-'}
 
@@ -20,12 +20,12 @@ def match(ch,axtfile, hfile):
 	l=l.strip().split(' ') #format: 0# 1chr_hg 2start_hg 3end_hg 4chr_pt 5start_pt 6end_pt 7strand 8blastscore
 	#start reading through the file
 	while l != ['']:
-		while l[4] not in ptChrs:
+		while l[4] not in ptChrs or l[1] != 'chr'+ch:
 			#read on
 			for meh in xrange(4):
 				l=axt.readline().strip().split(' ')
 			if l == ['']: break
-		#here they're a matched line
+		#here they're matched
 		if l == ['']: break
 		start = int(l[2]); end = int(l[3])
 		#size = end-start+1
@@ -40,7 +40,6 @@ def match(ch,axtfile, hfile):
 					ptRef = ptseq[i]
 					hgRef = hgseq[i]
 					Hanc.write('%s\t%s\t%s\t%s\n'%(ch,pos,hgRef,ptRef))
-
 			''' #Gaps in human and in chimp cannot be processed equally 
 			if hgseq[i] in Bases and ptseq[i] in Bases: #ignore gaps
 				anc = ptseq[i]
@@ -53,14 +52,11 @@ def match(ch,axtfile, hfile):
 	axt.close()
 	Hanc.close()
 
-
-
-
 def main():
 	ch = sys.argv[1]
-  path = sys.argv[2]
-	axtfile = '%s/hg18_to_panTro2/chr%s.hg18.panTro2.net.axt.gz' % (path,ch)
-	hfile = '%s/hg18matchPT2/chr%s.hg18.pT2.txt.gz' %(path,ch)
+  	path = sys.argv[2]
+	axtfile = '%s/hg18_to_panTro2/hg38.panTro2.net.axt.gz' % (path)
+	hfile = '%s/hg38matchPP2/chr%s.hg18.pT2.txt.gz' %(path,ch)
 	match(ch, axtfile, hfile)
 	print 'Chrmosome', ch, 'done at', datetime.now()
 
